b808b8131b4723de174654ba24f54cbbfd535fbc
Dare? <117949920+LutegW@users.noreply.github.com>
2022-12-06 09:33:23 +0000

    Add files via upload

--- bot/__init__.py
+++ bot/__init__.py
@@ -385,8 +385,12 @@ IGNORE_PENDING_REQUESTS = IGNORE_PENDING_REQUESTS.lower() == 'true'
 AS_DOCUMENT = environ.get('AS_DOCUMENT', '')
 AS_DOCUMENT = AS_DOCUMENT.lower() == 'true'
 
-CUSTOM_FILENAME = environ.get('CUSTOM_FILENAME', "[@mirror_gan]")
-CUSTOM_FILENAME = CUSTOM_FILENAME.lower() == 'true'
+try:
+    CUSTOM_FILENAME = getConfig('CUSTOM_FILENAME')
+    if len(CUSTOM_FILENAME) == 0:
+        raise KeyError
+except:
+    CUSTOM_FILENAME = None
 
 EQUAL_SPLITS = environ.get('EQUAL_SPLITS', '')
 EQUAL_SPLITS = EQUAL_SPLITS.lower() == 'true'
