11e9a3d9ba913b1048a6448e977490efe24db693
Dare? <117949920+LutegW@users.noreply.github.com>
2022-12-06 01:13:20 +0000

    Add files via upload

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -1,28 +1,66 @@
 from threading import Thread
-from telegram.ext import CommandHandler
+from telegram.ext import CommandHandler, CallbackQueryHandler
 
-from bot import LOGGER, dispatcher
+from bot import LOGGER, dispatcher, config_dict
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
-from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, auto_delete_message, sendFile, deleteMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, editMessage, sendMarkup, sendFile, deleteMessage
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
+from bot.helper.telegram_helper.button_build import ButtonMaker
 
-def list_drives(update, context):
-    try:
-        key = update.message.text.split(" ", maxsplit=1)[1]
+def list_buttons(update, context):
+    user_id = update.message.from_user.id
+    if len(context.args) == 0:
+        return sendMessage('⚠️ Gunakan cara dibawah\n\n<b>Ketik: </b><code>/search NamaFile</code>\n\n<b>Contoh: </b><code>/search God of War</code>', context.bot, update.message)
+    buttons = ButtonMaker()
+    buttons.sbutton("🗃️️ Folders", f"types {user_id} folders")
+    buttons.sbutton("📁 Files", f"types {user_id} files")
+    buttons.sbutton("📚 Semua", f"types {user_id} both")
+    buttons.sbutton("❌ Batal", f"types {user_id} cancel")
+    button = buttons.build_menu(2)
+    sendMarkup(🔎 <b>Pilih metode pencarian!</b>', context.bot, update.message, button)
+
+def select_type(update, context):
+    query = update.callback_query
+    user_id = query.from_user.id
+    msg = query.message
+    key = msg.reply_to_message.text.split(" ", maxsplit=1)[1]
+    data = query.data
+    data = data.split()
+    if user_id != int(data[1]):
+        return query.answer(text="⚠️ Bukan buat lu bang!", show_alert=True)
+    elif data[2] == 'cancel':
+        query.answer()
+        return editMessage("⚠️ Pencarian telah dibatalkan!", msg)
+    query.answer()
+    item_type = data[2]
+    editMessage(f"🔎 <b>Sedang Mencari:</b> <code><i>{key}</i></code>", msg)
+    Thread(target=_list_drive, args=(context.bot, key, msg, item_type)).start()
+
+def _list_drive(bot, key, bmsg, item_type):
+    if config_dict['TELEGRAPH_STYLE']:
+
         LOGGER.info(f"listing: {key}")
-        bmsg = sendMessage(f"🔎 <b>Sedang Mencari:</b> <code><i>{key}</i></code>", context.bot, update.message)
         gdrive = GoogleDriveHelper()
-        cap, f_name = gdrive.drive_list(key, isRecursive=True)
-        if cap:
-            deleteMessage(context.bot, bmsg)
-            sendFile(context.bot, bmsg.reply_to_message, f_name, cap)
+        msg, button = gdrive.drive_list(key, isRecursive=True, itemType=item_type)
+        if button:
+            editMessage(msg, bmsg, button)
         else:
-            editMessage(f'<b>📝 Tidak menemukan hasil dari:</b> <code><i>{key}</i></code>', bmsg)
-    except Exception as err:
-        LOGGER.error(f"listing error: {err}")
-        bmsg = sendMessage('⚠️ Gunakan cara dibawah\n\n<b>Ketik: </b><code>/search NamaFile</code>\n\n<b>Contoh: </b><code>/search God of War</code>', context.bot, update.message)
-        Thread(target=auto_delete_message, args=(context.bot, update.message, bmsg)).start()
+            editMessage(f'<b>📝 Tidak menemukan hasil dari:</b> <code><i>{key}</i></code>'', bmsg)
 
-list_handler = CommandHandler(BotCommands.ListCommand, list_drives, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+    else:
+
+        LOGGER.info(f"listing: {key}")
+        gdrive = GoogleDriveHelper()
+        cap, f_name = gdrive.drive_list(key, isRecursive=True, itemType=item_type)
+        if cap:
+            deleteMessage(bot, bmsg)
+            sendFile(bot, bmsg.reply_to_message, f_name, cap)
+        else:
+            editMessage(f'<b>📝 Tidak menemukan hasil dari:</b> <code><i>{key}</i></code>'', bmsg)
+
+list_handler = CommandHandler(BotCommands.ListCommand, list_buttons,
+                              filters=CustomFilters.authorized_chat | CustomFilters.authorized_user, run_async=True)
+list_type_handler = CallbackQueryHandler(select_type, pattern="types", run_async=True)
 dispatcher.add_handler(list_handler)
+dispatcher.add_handler(list_type_handler)
