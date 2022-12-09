d65aa5786e0bb7f54e1af44c1069f8303772a5e7
Dare? <117949920+LutegW@users.noreply.github.com>
2022-12-09 08:57:15 +0000

    Typo

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -11,7 +11,7 @@ import cloudscraper
 from bs4 import BeautifulSoup
 from base64 import standard_b64encode, b64decode
 from http.cookiejar import MozillaCookieJar
-from os import path as ospatch
+from os import path as ospath
 from playwright.sync_api import Playwright, sync_playwright, expect
 
 from bot import LOGGER, config_dict
